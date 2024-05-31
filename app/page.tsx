7cc895d4bcc6d0ecefc93a82344efc02edd207df
Goune <71518163+Goune1@users.noreply.github.com>
2024-05-31 13:36:23 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,8 +50,8 @@ export default function Home() {
           </div>
           
         </div>
-        <div className="">
-          <img src="images/r9.jpg" className="rounded-full w-72 h-24 lg:w-72 lg:h-72" alt="" />
+        <div className="ml-8">
+          <img src="images/r9.jpg" className="rounded-full w-72 h-24 lg:w-72 lg:h-72" alt="profile picture" />
         </div>
       </div>
 
