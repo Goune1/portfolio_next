891670ee11e5177b85821d17e753407538778d8c
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-04 21:28:34 +0200

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://portfolio.gounevps.com', // Remplacez par l'URL de votre site
+    generateRobotsTxt: true, // (Optionnel) Crée également un fichier robots.txt
+    // ...autres options si nécessaire
+};
